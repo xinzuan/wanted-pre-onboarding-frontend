a0a885b80076e785603e64fb297ca1f0f1d7bbf8
xinzuan <veldazelda15@gmail.com>
2023-06-10 19:57:11 +0700

    change to client side routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,5 @@
-
-import './App.css';
-import {BrowserRouter, Route, Switch} from 'react-router-dom';
+import React from 'react';
+import { HashRouter as Router, Route, Switch } from 'react-router-dom';
 import Login from "./components/login.component";
 import SignUp from './components/signup.component';
 import Home from './components/home.component';
@@ -11,16 +10,15 @@ import "bootstrap/dist/js/bootstrap.bundle.min.js";
 
 function App() {
   return (
-    <BrowserRouter>
-    <Switch>
-      <Route exact path="/signin" component={Login}/>
-      <Route exact path="/signup" component={SignUp}/>
-      <TodoProvider>
-        <PrivateRoute exact path="/todo" component={Home}/>
-      </TodoProvider>
- 
+    <Router>
+      <Switch>
+        <Route exact path="/signin" component={Login} />
+        <Route exact path="/signup" component={SignUp} />
+        <TodoProvider>
+          <PrivateRoute exact path="/todo" component={Home} />
+        </TodoProvider>
       </Switch>
-  </BrowserRouter>
+    </Router>
   );
 }
 
